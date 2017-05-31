9f42cb9d61a6e03169ed67d8484e0c7ac261c778
Hudson Pereira <hudson.byte@gmail.com>
2017-05-31 11:52:16 -0300

    first commit

---
+++ app/Post.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
